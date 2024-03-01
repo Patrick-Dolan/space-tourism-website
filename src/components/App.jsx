c95575c961f95b6fbf44e5cf19aaf2e46215b26c
Patrick Dolan <dolanp1992@gmail.com>
2024-03-01 09:17:56 -0800

    add and configure react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import DestinationPage from "./pages/DestinationPage";
+import CrewPage from "./pages/CrewPage";
+import Navbar from "./Navbar";
+import { Routes, Route } from "react-router-dom";
+import TechnologyPage from "./pages/TechnologyPage";
+
 function App() {
   return (
-    <div className="container mx-6">
-      <p>App component</p>
+    <div className="container px-6 text-primary">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/destination" element={<DestinationPage />} />
+        <Route path="/crew" element={<CrewPage />} />
+        <Route path="/technology" element={<TechnologyPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
