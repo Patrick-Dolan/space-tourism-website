4d292bb815efe483f6e82cd7414d027d5690efeb
Patrick Dolan <dolanp1992@gmail.com>
2024-03-05 09:32:52 -0800

    Add destination selection functionality and update styles

--- src/components/pages/DestinationPage.jsx
+++ src/components/pages/DestinationPage.jsx
@@ -1,6 +1,35 @@
+import PageHeader from "../shared/PageHeader";
+import data from "../../data.json";
+import { useState } from "react";
+
 function DestinationPage() {
+  const { destinations } = data;
+  const [selectedDestination, setSelectedDestination] = useState(destinations[0]);
+
   return (
-    <div>DestinationPage Component</div>
+    <>
+      <PageHeader number="01" title="Pick your destination" />
+      <img className="h-planet w-planet mx-auto mt-8 mb-6" src={"./src" + selectedDestination.images.png.substring(1)} />
+      <div className="flex flex-row justify-center gap-7 mb-5">
+      {destinations.map((destination) => (
+          <div key={destination.name}>
+            <button
+              onClick={() => setSelectedDestination(destination)}
+              className={`block uppercase font-primary-condensed text-sub ${destination.name === selectedDestination.name ? "text-primary border-b-3 pb-2" : "text-secondary"}  tracking-nav`}
+            >
+              {destination.name}
+            </button>
+          </div>
+      ))}
+      </div>
+      <h2 className="uppercase text-center">{selectedDestination.name}</h2>
+      <p className="text-center text-secondary">{selectedDestination.description}</p>
+      <hr className="border-primary my-8" />
+      <p className="uppercase text-center tracking-subhead font-primary-condensed text-secondary text-sub">Avg. Distance</p>
+      <p className="text-center text-lg-h5 font-secondary mt-3 uppercase">{selectedDestination.distance}</p>
+      <p className="uppercase text-center tracking-subhead font-primary-condensed text-secondary text-sub mt-8">EST. Travel Time</p>
+      <p className="text-center text-lg-h5 font-secondary mt-3 uppercase mb-14">{selectedDestination.travel}</p>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,11 @@ export default {
         secondary: "var(--secondary)",
         tertiary: "var(--tertiary)",
       },
+      borderColor: {
+        primary: "var(--border)"
+      },
       fontSize: {
+        sub: "0.875rem",
         "lg-h1": "9.375rem",
         "lg-h2": "6.25rem",
         "lg-h3": "3.5rem",
@@ -26,12 +30,13 @@ export default {
       },
       fontFamily: {
         primary: ["Barlow", "sans-serif"],
-        "secondary-condensed": ["Barlow Condensed", "sans-serif"],
+        "primary-condensed": ["Barlow Condensed", "sans-serif"],
+        secondary: ["Bellefair", "serif"],
       },
       letterSpacing: {
         nav: "0.169rem",
         "md-h5": "0.294rem",
-        subhead: "2.35rem"
+        subhead: "0.147rem"
       },
       spacing: {
         29: "7.25rem",
@@ -40,7 +45,30 @@ export default {
       },
       leading: {
         md: "1.75rem"
-      }
+      },
+      backgroundImage: {
+        "home-mobile": "url('./assets/home/background-home-mobile.jpg')",
+        "home-tablet": "url('./assets/home/background-home-tablet.jpg')",
+        "home-desktop": "url('./assets/home/background-home-desktop.jpg')",
+        "destination-mobile": "url('./assets/destination/background-destination-mobile.jpg')",
+        "destination-tablet": "url('./assets/destination/background-destination-tablet.jpg')",
+        "destination-desktop": "url('./assets/destination/background-destination-desktop.jpg')",
+        "crew-mobile": "url('./assets/crew/background-crew-mobile.jpg')",
+        "crew-tablet": "url('./assets/crew/background-crew-tablet.jpg')",
+        "crew-desktop": "url('./assets/crew/background-crew-desktop.jpg')",
+        "technology-mobile": "url('./assets/technology/background-technology-mobile.jpg')",
+        "technology-tablet": "url('./assets/technology/background-technology-tablet.jpg')",
+        "technology-desktop": "url('./assets/technology/background-technology-desktop.jpg')",
+      },
+      height: {
+        planet: "10.625rem",
+      },
+      width: {
+        planet: "10.625rem",
+      },
+      borderWidth: {
+        3: "3px"
+      },
     },
   },
   plugins: [],
