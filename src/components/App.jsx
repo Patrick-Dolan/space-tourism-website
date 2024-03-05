c94323e6e0fee810309f0947ba7ad13137fb3374
Patrick Dolan <dolanp1992@gmail.com>
2024-03-05 09:32:15 -0800

    set up conditional background images for different web pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,33 @@ import NotFoundPage from "./pages/NotFoundPage";
 import DestinationPage from "./pages/DestinationPage";
 import CrewPage from "./pages/CrewPage";
 import Navbar from "./Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
+  const location = useLocation();
+
+  let bgImage;
+
+  switch (location.pathname) {
+    case "/":
+      bgImage = "bg-home-mobile";
+      break;
+    case "/destination":
+      bgImage = "bg-destination-mobile";
+      break;
+    case "/crew":
+      bgImage = "bg-crew-mobile";
+      break;
+    case "/technology":
+      bgImage = "bg-technology-mobile";
+      break;
+    default:
+      bgImage = "bg-home-mobile";
+  }
+
   return (
-    <div className="container p-6 text-primary">
+    <div className={`container p-6 text-primary min-h-screen ${bgImage} bg-fixed bg-no-repeat bg-cover bg-center`}>
       <Navbar />
       <main>
         <Routes>
