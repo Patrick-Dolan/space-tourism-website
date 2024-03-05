a43810c268deeb4775508d34e37c01b8c4bb8cfe
Patrick Dolan <dolanp1992@gmail.com>
2024-03-05 09:43:45 -0800

    update font styles in HomePage and PageHeader components

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     <>
       <div className="flex flex-col justify-between gap-20">
         <div>
-          <h5 className="uppercase font-secondary-condensed text-center text-secondary pt-12 md:tracking-md-h5">So, you want to travel to</h5>
+          <h5 className="uppercase font-primary-condensed text-center text-secondary pt-12 md:tracking-md-h5">So, you want to travel to</h5>
           <h1 className="uppercase text-center">Space</h1>
           <p className="text-center md:pt-12 text-secondary">Let&apos;s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we&apos;ll give you a truly out of this world experience!</p>
         </div>
--- src/components/shared/PageHeader.jsx
+++ src/components/shared/PageHeader.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function PageHeader({number, title}) {
   return (
     <>
-      <h5 className="uppercase font-secondary-condensed text-center mt-6">
+      <h5 className="uppercase font-primary-condensed text-center mt-6">
         <span className="opacity-25 pr-4 font-bold">{number}</span>
         {title}
       </h5>
