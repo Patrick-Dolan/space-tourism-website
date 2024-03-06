b3ae0457d8af7da3d5325123b64a9efcf2aff621
Patrick Dolan <dolanp1992@gmail.com>
2024-03-06 08:33:12 -0800

    refactor how image positioning works with other elements

--- src/components/pages/TechnologyPage.jsx
+++ src/components/pages/TechnologyPage.jsx
@@ -9,10 +9,12 @@ function TechnologyPage() {
   return (
     <>
       <PageHeader number="03" title="Space launch 101" />
-      <img src={"./src" + selectedTechnology.images.landscape.substring(1)} alt={`${selectedTechnology.name}`} className="mt-8 absolute left-0 min-w-screen" />
-      <div className="flex justify-center gap-7 mt-48">
+      {/* The following div is an empty div used to make space for the absolutely positioned image below.*/}
+      <div className="mt-8" style={{ paddingTop: '45.3%' }} /> 
+      <img src={"./src" + selectedTechnology.images.landscape.substring(1)} alt={`${selectedTechnology.name}`} className="mt-14 absolute left-0 top-0 w-full" />
+      <div className="flex justify-center gap-7 mt-12">
         {technology.map((tech, index) => (
-          <div key={tech.name} className="mt-8">
+          <div key={tech.name}>
             <button
               onClick={() => setSelectedTechnology(tech)}
               className={`block font-secondary text-base text-sub h-10 w-10 rounded-full border border-white leading-10 ${tech.name === selectedTechnology.name ? "text-tertiary bg-white" : " border-opacity-25"}`}
